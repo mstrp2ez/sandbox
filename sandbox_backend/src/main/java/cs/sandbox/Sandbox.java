644c271368983baf1b49f16336f47129ea40ecf1
Christoffer Södergren <mstrp2ez@gmail.com>
2017-10-05 22:37:49 +0200

    Initial commit

--- sandbox_backend/src/main/java/cs/sandbox/Sandbox.java
+++ sandbox_backend/src/main/java/cs/sandbox/Sandbox.java
@@ -1,10 +1,27 @@
 package cs.sandbox;
 
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
 public class Sandbox {
 
-	public static void main(String[] args) {
-		System.out.println("Hello!");
-
+	public void run(){
+		
+		
+		
 	}
 
+	public static void main(String[] args) {
+		new Sandbox().run();
+	}
 }
